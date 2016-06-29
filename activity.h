0086a53a9cc625bd9e5c3e398d12cb53607cf98a
Andrey Bezborodov <andrey@wavecon.ru>
2016-06-29 10:17:08 +0300

    First version

---
+++ activity.h
@@ -0,0 +1,138 @@
+#pragma once
+#include <thread>
+
+/**
+ * @class activity
+ * @author Andrey Bezborodov
+ * @brief Implement activity object
+ */
+template <class T>
+class activity {
+public:
+  //! Function callback
+  class callback{
+  public:
+    virtual ~callback(){}
+    virtual void operator()() = 0;
+  };
+
+  //! Callback adapter
+  template< typename OwnerClass >
+  class adapter : public callback{
+  public:
+    //! Exported function prototype
+    typedef void (OwnerClass::*function)();
+  private:
+    //! Pointer to owner class
+    OwnerClass* _class_ptr;
+
+    //! Pointer to function with argument
+    function    _callback;
+  public:
+    //! Constructor
+    adapter(OwnerClass * class_ptr, typename adapter::function func)
+    : _class_ptr(class_ptr)
+    , _callback(func)
+    {}
+    //! Call function
+    virtual void operator()(){
+      (_class_ptr->*_callback)();
+    }
+  };
+
+public:
+  //! Constructor
+  activity(T* class_ptr, typename adapter<T>::function func)
+  : _IsRunning(false)
+  , _Callback(new adapter<T>(class_ptr, func))
+  {}
+
+  //! Start activity
+  void start() {
+    _IsRunning = true;
+    pthread_create(&_Thread, NULL, &activity::__run__, this);
+  }
+
+  //! Stop activity
+  void stop() {
+    _IsRunning = false;
+    pthread_cancel(_Thread);
+    join(nullptr);
+  }
+
+  //! Check thread for cancelation
+  void __cancel_point__() {
+    pthread_testcancel();
+  }
+
+  //! Wait for activity end
+  int join(void** retval) {
+    return pthread_join(_Thread, retval);
+  }
+
+  //! Check is thread in running state
+  bool running() const {
+    return _IsRunning;
+  }
+
+  //! Yield processor time
+  void yield() const {
+    pthread_yield();
+  }
+private:
+
+  //! Thread entry adapter
+  static void* __run__(void* args) {
+    activity* item = reinterpret_cast<activity*>(args);
+    (*item->_Callback)();
+    item->_IsRunning = false;
+    return nullptr;
+  }
+
+private:
+  //! Thread object
+  pthread_t                 _Thread;
+
+  //! Is running
+  bool                      _IsRunning;
+
+  //! Pointer to callback function
+  std::shared_ptr<callback> _Callback;
+};
+
+/**
+ * @class runnable
+ * @author Andrey Bezborodov
+ * @brief Implement runnable class
+ */
+class runnable {
+public:
+  //! Constructor
+  runnable()
+  : _task(this, &runnable::run)
+  {}
+
+  //! Destructor
+  virtual ~runnable() {}
+
+  //! Run class
+  virtual void run() = 0;
+
+  //! Start
+  void start() {
+    _task.start();
+  }
+
+  //! Stop
+  void stop() {
+    _task.stop();
+  }
+
+  //! Running
+  bool running() {
+    return _task.running();
+  }
+private:
+  activity<runnable>  _task;
+};
+
